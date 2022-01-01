c7ddd22c81dd068697c765e1a6df27c3da9c2f6e
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2022-01-01 15:42:11 -0800

    succesfully used jest to test that DOM is manipulated as expected

--- force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++ force-app/main/default/lwc/unitTest/__tests__/sum.test.js
@@ -5,3 +5,8 @@ describe('sum', () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
+
+test('should not be greater than 3', ()=>{
+    let actual = sum(1,2);
+    expect(actual).not.toBeGreaterThan(3);
+});
